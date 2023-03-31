3a21ca56506a33086fbe92c09494970e3fcb3a34
OlehVelychko <alligator3105@gmail.com>
2023-03-31 12:23:54 +0300

    test commit from IDEA Ultimate

---
+++ 1.JavaSyntax/src/forTest/mathRandom.java
@@ -0,0 +1,10 @@
+package forTest;
+
+public class mathRandom {
+    public static void main(String[] args) {
+        double result = Math.random();
+        System.out.println(result);
+        int number = (int) (Math.random() * 10);
+        System.out.println(number);
+    }
+}
